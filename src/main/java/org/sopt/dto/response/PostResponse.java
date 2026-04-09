9bb6d3f12be696aad504933f4367a3142b1d391f
sophie-24 <gyuri5578@naver.com>
2026-04-09 10:59:19 +0900

    feat: 게시글 상세 정보 반환을 위한 PostResponse DTO 추가

---
+++ src/main/java/org/sopt/dto/response/PostResponse.java
@@ -0,0 +1,19 @@
+package org.sopt.dto.response;
+//id랑 message만 받을 수 있는 CreatePostResponse와 달리, 사용자가 게시글을 수정하고 작성하고 반환하려면
+//id, title, content, author, createdat을 받을 수 있어야 한다고 생각이 들어서
+//postresponse를 만들어 보았습니다.
+public class PostResponse {
+    public Long id;
+    public String title;
+    public String content;
+    public String author;
+    public String CreatedAt;
+
+    public PostResponse(Long id, String title, String content, String author, String CreatedAt){
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.author = author;
+        this.CreatedAt = CreatedAt;
+    }
+}
