9ea5eda993de6079cd9ebff3aa88b7482ae2f537
sophie-24 <gyuri5578@naver.com>
2026-04-25 16:10:08 +0900

    [fix] JpaRepository 상속 오류 해결

--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package org.sopt.repository;
 import org.sopt.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -8,29 +9,6 @@ import java.util.Optional;
 
 /*3주차 내용 : 직접 구현한 인메모리 저장소 -> 인터페이스만 선언하면 된다.*/
 @Repository
-public class PostRepository extends JpaRepository<Post, Long>{
+public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
-
-//{
-//    private final List<Post> postList = new ArrayList<>();
-//    private Long nextId = 1L; //id long 타입이니까 L 붙여봤어요
-//    public Post save(Post post) {
-//        postList.add(post);
-//        return post;
-//    }
-//    public List<Post> findAll() {
-//        return postList;
-//    }
-//    public Optional<Post> findById(Long id) {  // null 대신 Optional
-//        return postList.stream()
-//                .filter(p -> p.getId().equals(id))
-//                .findFirst();
-//    }
-//    public boolean deleteById(Long id){
-//        return postList.removeIf(p -> p.getId().equals(id));
-//    }
-//    public Long generateId(){ // 익명 1, 익명 2....
-//        return nextId++;
-//    }
-//}
