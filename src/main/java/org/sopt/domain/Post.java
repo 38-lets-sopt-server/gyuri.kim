e977228ec3de9c4bb095c0ef5f2b8831220b75b3
sophie-24 <gyuri5578@naver.com>
2026-04-25 15:33:34 +0900

    [feat] User 엔티티 생성 및 레포지토리 추가, Jpa 기본 생성자 반영

--- src/main/java/org/sopt/domain/Post.java
+++ src/main/java/org/sopt/domain/Post.java
@@ -1,30 +1,39 @@
 package org.sopt.domain;
 
-public class Post {
-    private Long id;          // 게시글 상세 화면 — 특정 게시글 식별용
-    private String title;     // 목록, 상세, 글쓰기 화면 — 제목
-    private String content;   // 목록(미리보기), 상세(전체) 화면 — 내용
-    private String author;    // 목록, 상세 화면 — 글쓴이
-    private String createdAt; // 목록, 상세 화면 — 작성 시각
+import com.fasterxml.jackson.annotation.JsonTypeId;
 
-    public Post(Long id, String title, String content, String author, String createdAt) {
-        this.id = id;
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+    //private String author;
+    //private String createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)  // User : Post = 1 : N
+    @JoinColumn(name = "user_id")       // post 테이블에 user_id FK 컬럼이 생겨요
+    private User user;
+
+    protected Post(){}
+
+    public Post(String title, String content, String author, String createdAt) {
         this.title = title;
         this.content = content;
         this.author = author;
         this.createdAt = createdAt;
     }
-
-    public Long getId() { return id; }
-    public String getTitle() { return title; }
-    public String getContent() { return content; }
-    public String getAuthor() { return author; }
-    public String getCreatedAt() { return createdAt; }
-
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
     }
+
+    public Long getId() { return this.id; }
+    public String getTitle() { return this.title; }
+    public String getContent() { return content; }
+    public String getAuthor() { return author; }
+    public String getCreatedAt() { return createdAt; }
+
 
     public String getInfo() {
         return "[" + id + "] " + title + " - " + author + " (" + createdAt + ")\n" + content;
--- src/main/java/org/sopt/repository/PostRepository.java
+++ src/main/java/org/sopt/repository/PostRepository.java
@@ -6,26 +6,31 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+/*3주차 내용 : 직접 구현한 인메모리 저장소 -> 인터페이스만 선언하면 된다.*/
 @Repository
-public class PostRepository {
-    private final List<Post> postList = new ArrayList<>();
-    private Long nextId = 1L; //id long 타입이니까 L 붙여봤어요
-    public Post save(Post post) {
-        postList.add(post);
-        return post;
-    }
-    public List<Post> findAll() {
-        return postList;
-    }
-    public Optional<Post> findById(Long id) {  // null 대신 Optional
-        return postList.stream()
-                .filter(p -> p.getId().equals(id))
-                .findFirst();
-    }
-    public boolean deleteById(Long id){
-        return postList.removeIf(p -> p.getId().equals(id));
-    }
-    public Long generateId(){ // 익명 1, 익명 2....
-        return nextId++;
-    }
+public class PostRepository extends JpaRepository<Post, Long>{
+
 }
+
+//{
+//    private final List<Post> postList = new ArrayList<>();
+//    private Long nextId = 1L; //id long 타입이니까 L 붙여봤어요
+//    public Post save(Post post) {
+//        postList.add(post);
+//        return post;
+//    }
+//    public List<Post> findAll() {
+//        return postList;
+//    }
+//    public Optional<Post> findById(Long id) {  // null 대신 Optional
+//        return postList.stream()
+//                .filter(p -> p.getId().equals(id))
+//                .findFirst();
+//    }
+//    public boolean deleteById(Long id){
+//        return postList.removeIf(p -> p.getId().equals(id));
+//    }
+//    public Long generateId(){ // 익명 1, 익명 2....
+//        return nextId++;
+//    }
+//}
